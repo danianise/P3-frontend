ce909f1890994db5775a7090d831b03382a93713
YogaLiao <liaoyujia0702@gmail.com>
2022-05-21 16:05:50 -0500

    add CRUD

--- src/App.js
+++ src/App.js
@@ -14,17 +14,39 @@ function App() {
   const [stockData, setstockData] = useState(null)
   const [dbData, setdbData] = useState(null)
 
+  const getDbData = () => {
+    fetch(dbURL)
+      .then(res => res.json())
+      .then(data => setdbData(data))
+
+  }
+
+  const updateDbData = async (data, id) => {
+    await fetch(URL + id, {
+        method: 'put',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    getDbData();
+  }
+  
+  const deleteDbData = async id => {
+    await fetch(URL + id, {
+        method: 'delete',
+    })
+    // Update the list
+    getDbData();
+}
+  
   useEffect( ()=> {
     fetch(url)
       .then(res => res.json())
       .then(data => setstockData(data))
   }, [])
 
-  useEffect(() => {
-    fetch(dbURL)
-      .then(res => res.json())
-      .then(data => setdbData(data))
-  }, [])
+  useEffect(() => getDbData(), [])
 
   let test = "Meta Data"
   let test2 = "2. Symbol"
@@ -35,9 +57,18 @@ function App() {
       {stockData ? console.log(stockData) : ""}
       {dbData ? console.log(dbData) : ""}
     <Routes>
-        <Route path='/portfolio/:symbol' element={<Stock /> } />
-        <Route path='/portfolio' element={<Portfolio/>} />
-        <Route path='/portfolio/watchlist' element={<Watchlist />} />
+        <Route path='/portfolio/:symbol' element={<Stock 
+        dbData = {dbData}
+        updateDbData={updateDbData}
+        deleteDbData = {deleteDbData} />} />
+        <Route path='/portfolio' element={<Portfolio
+        dbData = {dbData}
+        updateDbData={updateDbData}
+        deleteDbData = {deleteDbData}/>} />
+        <Route path='/portfolio/watchlist' element={<Watchlist
+        dbData = {dbData}
+        updateDbData={updateDbData}
+        deleteDbData = {deleteDbData}/>} />
     </Routes>
     </>
   );
