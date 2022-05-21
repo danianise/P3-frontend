64ac58b8a11a27115942810b2c868d337b0692d2
YogaLiao <liaoyujia0702@gmail.com>
2022-05-21 14:48:00 -0500

    add links to detail page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            <a href='./'>Home</a><br></br>
+            <a href='/'>Home</a><br></br>
         </div>
     )
 }
