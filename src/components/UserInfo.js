b4c4a65bd951a223a65bac54487548d00ffa349e
danianise <danianisehoey@gmail.com>
2022-05-21 17:17:39 -0400

    links in portfolio page

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 function UserInfo({data}) {
+
   return (
-    <div>
+
+  !data ? "Loading"
+   : <div>
         <h4 className="userName">Username: {data[0].Username}</h4>
         <p>Cash Balance: ${data[0].CashBalance}</p>
     </div>
