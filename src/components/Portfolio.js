79a46d2fde1817698423d9d50552e7ade3fded88
danianise <danianisehoey@gmail.com>
2022-06-15 14:14:51 -0400

    Changed API key

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -20,8 +20,7 @@ function Portfolio({dbData}) {
         method: 'GET',
         url: `https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=${symbolStrings}`,
         headers: {
-            // 'x-api-key': 'GW8PTGhaii4nWR17s628j9t73Awi1MOpiy7NRyC4',
-            'x-api-key': 'SAt1ZodoG84fd4ZMP1Aq575wHne1e9ts5CUzkfLZ',
+            'x-api-key': 'bJycv9CnlPatYdKxJX3gS4k9mnRslOAL6V6Pp6v8',
             'Content-Type': 'application/json'
         }
     };
