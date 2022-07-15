ce2c0649245f4d44019acc0396b7f4796dca6195
danianise <danianisehoey@gmail.com>
2022-07-15 17:35:45 -0400

    Switching auth keys for API

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -21,8 +21,8 @@ function Portfolio({dbData}) {
         url: `https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=${symbolStrings}`,
         headers: {
             // 'x-api-key': 'bJycv9CnlPatYdKxJX3gS4k9mnRslOAL6V6Pp6v8',
-            'x-api-key': 'clgGSRIChw7cGlN5VFWyJ4bJTQdIDodsad4glnzZ',
-            // 'x-api-key': 'SAt1ZodoG84fd4ZMP1Aq575wHne1e9ts5CUzkfLZ',
+            // 'x-api-key': 'clgGSRIChw7cGlN5VFWyJ4bJTQdIDodsad4glnzZ',
+            'x-api-key': 'SAt1ZodoG84fd4ZMP1Aq575wHne1e9ts5CUzkfLZ',
             'Content-Type': 'application/json'
         }
     };
