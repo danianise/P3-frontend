5e55807c9a08f6c4a162ebcec553234c1ddde276
danianise <danianisehoey@gmail.com>
2023-03-09 13:18:02 -0500

    Changed width of media stock chart

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -194,7 +194,7 @@ function Stock(props) {
                                         }
                                         }
                                     }}
-                                    width='400px'
+                                    width='380px'
                                     yLabels={ {show: false} }
                                     xLabels={ {show: false} }
                                 />
